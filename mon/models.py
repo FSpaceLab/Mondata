f4bcef0673154498023200ef86e2d6a2e5ac696a
Bob Claster <navchalkin@ex.ua>
2018-04-10 20:19:54 +0300

    fixed bug

--- mon/models.py
+++ mon/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
 
 class Record(models.Model):
     volt = models.FloatField(null=True, blank=True)
     amper = models.FloatField(null=True, blank=True)
     watt = models.FloatField(null=True, blank=True)
     watt_hour = models.FloatField(null=True, blank=True)
-    dt = models.DateTimeField(default=datetime.now())
+    dt = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return str(self.dt)
